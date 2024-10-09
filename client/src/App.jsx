f1b457654685f8c4cbfe4191be32fd63ad9f76bc
Rang1114 <saengthongdee_R@silpakorn.edu>
2024-10-09 16:20:50 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Array from './components/array'
 import Nav from './components/navbar'
 import Footer from './components/footer'
 import './index.css'
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.js'
 import Dashbord from './HR/dashbord.jsx';
 import Createcost from './HR/createcost.jsx';
 import Repost from './HR/repost.jsx';
+import DashboardPage from './EMP/dashboard.jsx';
 
 const router = createBrowserRouter([
   {
@@ -27,11 +30,19 @@ const router = createBrowserRouter([
   {
     path: "repost",
     element: <Repost />
+  },
+  {
+    path: "DashboardPage",
+    element: <DashboardPage />
   }
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+
   </React.StrictMode>,
 );
